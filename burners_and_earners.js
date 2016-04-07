a6733f129ab3b66d079c7a6b55ff0f5e8191c3c8
MsEmma <emmaphiri@hotmail.com>
2016-04-07 12:47:13 +0200

    more answers

--- burners_and_earners.js
+++ burners_and_earners.js
@@ -15,10 +15,86 @@ exports.getData = function(input) {
 
 }
 
-exports.getMostData = function(data){
-  return data[data.length-1];
+exports.getMostData = function(data) {
+  return data[data.length - 1];
 }
 
-exports.getLeastData = function(data){
+exports.getLeastData = function(data) {
   return data[0];
 }
+
+exports.getAppMap = function(apps, inputArr) {
+
+  var array = [];
+
+  inputArr.forEach(function(str) {
+    array.push(str.match(/\w+/g));
+  })
+
+  var personApp = {};
+
+  array.forEach(function(arr) {
+    apps.forEach(function(app) {
+      arr.forEach(function(word) {
+        if (word === app) {
+          if (!personApp.hasOwnProperty(arr[0])) {
+            personApp[arr[0]] = [word];
+          } else {
+            personApp[arr[0]].push(word);
+          }
+        }
+      });
+    });
+  });
+
+  return personApp;
+}
+
+exports.getMostApps = function(personApp) {
+
+  var mostApps = 0;
+  most = "";
+
+  for (var person in personApp) {
+
+    if (personApp[person].length > mostApps) {
+      mostApps = personApp[person].length;
+      most = person;
+    }
+  }
+
+  return most;
+}
+
+exports.getFaceblockUsers = function(personApp) {
+
+  var faceblockUsers = [];
+
+  for (var person in personApp) {
+    if (personApp[person].indexOf("Faceblock") > -1) {
+      faceblockUsers.push(person);
+    }
+  }
+
+  return faceblockUsers;
+}
+
+exports.getFaceblockNonUsers = function(personApp) {
+
+  var faceblockNonUsers = [];
+
+  for (var person in personApp) {
+    if (personApp[person].indexOf("Faceblock") === -1) {
+      faceblockNonUsers.push(person);
+    }
+  }
+
+  return faceblockNonUsers;
+}
+
+var personApp = {
+  Walter: ['Faceblock', 'Instantgam'],
+  Jesse: ['Faceblock', 'Instantgam', 'Tooter', 'Snaptalk', 'LankedOn'],
+  Saul: ['Faceblock', 'Instantgam', 'LankedOn'],
+  Gus: ['Instantgam', 'Tooter']
+};
--- test/burners_and_earners_test.js
+++ test/burners_and_earners_test.js
@@ -1,7 +1,4 @@
 
-// Who uses the most apps?
-// Who uses Faceblock?
-// Who doesn’t use Faceblock?
 // For each person, estimate how much data each app uses. Use the data ratios from our friend at the network.
 
 var assert = require('assert');
@@ -35,6 +32,49 @@ describe("burners_and_earners", function() {
                                                 { name: 'Jesse', data: 300 } ]);
     assert.deepEqual(result, { name: 'Walter', data: 67 });
   });
+
+  it("should return person app map", function() {
+    result = burners_and_earners.getAppMap(["Faceblock", "Instantgam", "Tooter", "Snaptalk", "LankedOn"],
+    ["Walter sent: “Hello. I’ve got Faceblock and Instantgam. I’m too old for the other ones.”",
+    "Jesse sent: “Yo! I’ve got ‘em all. Tooter, Faceblock, Instantgam, Snaptalk, LankedOn.”",
+    "Saul sent: “I’m on LankedOn for Business. And I have an Instantgam account for my cats. Oh, and Faceblock for my ex-husbands.”",
+    "Gus sent: “I use Tooter and Instantgam. My customers aren’t on the others, so I can’t get them to engage with my brand there.”"]);
+    assert.deepEqual(result, { Walter: [ 'Faceblock', 'Instantgam' ],
+                               Jesse: [ 'Faceblock', 'Instantgam', 'Tooter', 'Snaptalk', 'LankedOn' ],
+                               Saul: [ 'Faceblock', 'Instantgam', 'LankedOn' ],
+                               Gus: [ 'Instantgam', 'Tooter' ] });
+  });
+
+  it("should return who uses the least data", function() {
+    result = burners_and_earners.getMostApps({ Walter: [ 'Faceblock', 'Instantgam' ],
+                                               Jesse: [ 'Faceblock', 'Instantgam', 'Tooter', 'Snaptalk', 'LankedOn' ],
+                                               Saul: [ 'Faceblock', 'Instantgam', 'LankedOn' ],
+                                               Gus: [ 'Instantgam', 'Tooter' ] });
+    assert.deepEqual(result, "Jesse");
+  });
+
+  it("should return who uses Faceblock?", function() {
+    result = burners_and_earners.getFaceblockUsers({ Walter: [ 'Faceblock', 'Instantgam' ],
+                                                     Jesse: [ 'Faceblock', 'Instantgam', 'Tooter', 'Snaptalk', 'LankedOn' ],
+                                                     Saul: [ 'Faceblock', 'Instantgam', 'LankedOn' ],
+                                                     Gus: [ 'Instantgam', 'Tooter' ] });
+    assert.deepEqual(result, ["Walter", "Jesse","Saul" ]);
+  });
+
+  it("should return who doesn’t use Faceblock?", function() {
+    result = burners_and_earners.getFaceblockNonUsers({ Walter: [ 'Faceblock', 'Instantgam' ],
+                                                     Jesse: [ 'Faceblock', 'Instantgam', 'Tooter', 'Snaptalk', 'LankedOn' ],
+                                                     Saul: [ 'Faceblock', 'Instantgam', 'LankedOn' ],
+                                                     Gus: [ 'Instantgam', 'Tooter' ] });
+    assert.deepEqual(result, ["Gus"]);
+  });
+
+
+
+
+
+
+
 
 
 
